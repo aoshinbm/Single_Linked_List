15af87e3d49b78d07b3064d6d353806184795cc7
aoshinbm <aoshinbm28797@gmail.com>
2023-02-26 18:14:38 +0530

    Added: Node added to the front of linked list

--- LinkedList.go
+++ LinkedList.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Value int
 	Next  *Node
@@ -7,4 +9,30 @@ type Node struct {
 
 type LinkedList struct {
 	Head *Node
+}
+
+func (l *LinkedList) Append(value int) {
+	newNode := &Node{Value: value, Next: l.Head}
+	l.Head = newNode
+}
+
+func (l *LinkedList) Display() {
+	if l.Head == nil {
+		fmt.Println("Linked List is empty.")
+		return
+	}
+	current := l.Head
+	fmt.Println("Linked List values:")
+	for current != nil {
+		fmt.Print("-->", current.Value)
+		current = current.Next
+	}
+}
+
+func main() {
+	list := &LinkedList{}
+	list.Append(10)
+	list.Append(20)
+	list.Append(30)
+	list.Display()
 }
