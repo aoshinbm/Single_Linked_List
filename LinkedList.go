9f2d8848d3d7d6095664aa437c0e7ea6bfda7926
aoshinbm <aoshinbm28797@gmail.com>
2023-02-26 18:27:45 +0530

    Added: Remove Node from the linkedlist

--- LinkedList.go
+++ LinkedList.go
@@ -11,11 +11,6 @@ type LinkedList struct {
 	Head *Node
 }
 
-func (l *LinkedList) Append(value int) {
-	newNode := &Node{Value: value, Next: l.Head}
-	l.Head = newNode
-}
-
 func (l *LinkedList) Display() {
 	if l.Head == nil {
 		fmt.Println("Linked List is empty.")
@@ -28,11 +23,42 @@ func (l *LinkedList) Display() {
 		current = current.Next
 	}
 }
+func (l *LinkedList) Append(value int) {
+	newNode := &Node{Value: value, Next: l.Head}
+	l.Head = newNode
+}
+
+func (l *LinkedList) DeleteElement(delete int) {
+	if l.Head == nil {
+		fmt.Println("LinkedList is empty")
+		return
+	}
+	if l.Head.Value == delete {
+		//deleting the head node
+		l.Head = l.Head.Next
+		return
+	}
+	/*	current := l.Head
+		for current.Next != nil {
+			if current.Next.Value == delete {
+				current.Next = current.Next.Next
+				return
+			}
+			current = current.Next
+		}
+	*/
+}
 
 func main() {
 	list := &LinkedList{}
 	list.Append(10)
 	list.Append(20)
 	list.Append(30)
+	list.Append(40)
+	list.Append(50)
+	list.Display()
+
+	fmt.Println(" ")
+	list.DeleteElement(50)
 	list.Display()
 }
